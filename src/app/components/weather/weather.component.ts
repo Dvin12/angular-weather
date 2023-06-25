64261883a83fd3ab9db266d246e383ed2616a756
Edvinas Venclavovic <edvinasvenclavovic@gmail.com>
2023-06-25 16:31:15 +0300

    added functionality/styling to the weather component

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from 'src/app/services/weather.service';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.scss']
+  styleUrls: ['./weather.component.scss'],
 })
-export class WeatherComponent {
+export class WeatherComponent implements OnInit {
+  currentWeather: any;
+  temperature: number = 0;
+  weatherImage: string = '';
+  weatherDescription: string = '';
+  constructor(private weatherService: WeatherService) {}
+  ngOnInit(): void {
+    this.weatherService.getWeatherData().subscribe({
+      next: (result) => {
+        this.currentWeather = result;
+        this.temperature = Math.round(this.currentWeather.main.temp);
 
+        const weatherIcon = this.currentWeather.weather[0].icon;
+        this.weatherImage = `assets/weather-icons/${weatherIcon}.png`;
+        this.weatherDescription = this.currentWeather.weather[0].description;
+      },
+
+      error: (error) => console.log(error.message),
+    });
+  }
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeatherService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getWeatherData() {
+    return this.http.get(
+      'https://api.openweathermap.org/data/2.5/weather?q=klaipeda&appid=df64afae23fae2a3b41d8fb056651f17&units=metric'
+    );
+  }
 }
